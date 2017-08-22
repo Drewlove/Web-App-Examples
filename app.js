8a362b63ad4a44de126d6c56b255036c35c58806
Drew Love <davidandrewlove84@gmail.com>
2017-08-22 13:07:54 -0400

    functionality to + button

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-let main = function(){
+var main = function(){
 	"use strict"; 
 
 	$(".comment-input button").on("click", function(event){
-	console.log("Hello World");
+	var $new_comment; 
+
+	if($(".comment-input input").val() !== ""){
+		$new_comment = $("<p>").text($(".comment-input input").val());
+		$(".comments").append($new_comment);
+		$(".comment-input input").val(""); 
+	}
+}); 
+	$(".comment-input input").on("keypress", function (event){
+		var $new_comment;
+		if(event.keyCode === 13){ 
+			if($(".comment-input input").val() !== ""){
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$(".comments").append($new_comment);
+				$(".comment-input input").val(""); 
+	}
+		}
+
 	});
-	//$(".comments").append("<p>"+$("input").val()+"</p>")
-}; 
+}
+
+	// $(".comment-input button").on("click", function(event){
+	// 	$(".comments").append("<p>"+$("input").val()+"</p>")
+
 $(document).ready(main);
 
 
