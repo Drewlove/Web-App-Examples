31fdcec18f1ff8ead02b8123d88e32813fa4c0be
Drew Love <davidandrewlove84@gmail.com>
2017-08-22 12:14:38 -0400

    javascript added

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 let main = function(){
-	"use strict";  
-}
+	"use strict"; 
+
+	$(".comment-input button").on("click", function(event){
+	console.log("Hello World");
+	});
+	//$(".comments").append("<p>"+$("input").val()+"</p>")
+}; 
+$(document).ready(main);
+
+
+//test
+
+
+
+
